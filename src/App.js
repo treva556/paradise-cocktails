681087a98dc3d29fa33f89837d01ef77f4fdf12d
treva556 <marktreva92@gmail.com>
2024-07-13 21:11:36 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import pic66 from './assets/pic66.jpeg'; 
+import Nav from './components/nav';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <header className="p-6">
+        <Nav/>
+        <img src={pic66} className='w-full md:w-full lg:w-full rounded-3xl object-cover' alt="Description of the image" />
+        <div className='bg-red-600 p-6 mt-2 rounded-2xl'>
+          hh
+        </div>
       </header>
+      <div className=' pb '>
+        pwww
+        <div className=' pb p-20'>
+        pwww
+      </div>
+      <div className='p-20'>
+        pwww
+      </div>
+
+<p>jjj</p>
+
+
+
+      </div>
     </div>
   );
 }
---
+++ src/components/nav.js
@@ -0,0 +1,15 @@
+///
+
+import React from 'react';
+import '../App.css';
+
+function Nav() {
+  return (
+    <div className="App">
+    <p className=' fixed w-full flex  mt-3 ml-2 mr-2 rounded-lg  h-10 mb-2 text-3xl text-red-700'> nav </p> 
+    </div>
+  );
+}
+
+export default Nav;
+

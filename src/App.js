651ed0b81da6f73ffce9b809ea4c3f0ef4328f5b
treva556 <marktreva92@gmail.com>
2024-07-14 01:43:17 +0300

    start

--- src/App.js
+++ src/App.js
@@ -8,13 +8,24 @@ function App() {
     <div className="App">
        <header className="p-6">
         <Nav/>
-        <img src={pic66} className='w-full md:w-full lg:w-full rounded-3xl object-cover' alt="Description of the image" />
-        <div className='bg-red-600 p-6 mt-2 rounded-2xl'>
-          hh
+        <div className="relative w-full h-80 rounded-3xl">
+            <img
+                src={pic66}
+                alt="Example"
+                className="w-full h-full object-cover rounded-3xl"
+            />
+            <div className="absolute inset-0 flex items-center justify-center text-white font-bold text-lg">
+                MARLOCA WEST BEACH CLUB
+            </div>
+        </div>
+        <div className='bg-red-500 p-6 mt-3 rounded-2xl italic text-white'>
+          Experience the ultimate bonanaza
         </div>
       </header>
-      <div className=' pb '>
-        pwww
+      <div className=' pb font-mono '>
+        <p className=' p-6'>
+        MARLOCA EAST BEACH CLUB is a destination and place to experience the beach in a jovial way. Where we have beautiful sunsets, great music, direct access to the beach, water sports activities and drinks prepared by the most talented mixologists
+        </p>
         <div className=' pb p-20'>
         pwww
       </div>
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,15 +1,43 @@
 ///
 
 import React from 'react';
-import '../App.css';
 
 function Nav() {
   return (
     <div className="App">
-    <p className=' fixed w-full flex  mt-3 ml-2 mr-2 rounded-lg  h-10 mb-2 text-3xl text-red-700'> nav </p> 
+      <nav className="border-gray-200">
+        <div className="max-w-screen-xl mx-auto flex justify-end">
+          <ul className="flex space-x-4 p-1 md:p-0">
+            <li>
+              <a
+                href="/"
+                className="block py-2 px-3 text-black rounded hover:bg-gray-100 dark:text-black"
+                aria-current="page"
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="/services"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-black"
+              >
+                Services
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-black"
+              >
+                About
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 }
 
 export default Nav;
-
